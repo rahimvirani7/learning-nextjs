d42e6b9b47f11eb77e98272e70f807c7a0db6a01
Rahim Virani <rahz616@gmail.com>
2023-07-03 12:26:39 -0400

    Adding generateStaticParams function

--- app/movie-list/Movie.jsx
+++ app/movie-list/Movie.jsx
@@ -6,7 +6,7 @@ export default function Movie({ title, id, poster_path, release_date }) {
     // console.log(poster_path);
 
     return (
-        <Link href={`movie-list/${id}`} className='p-4 my-2'>
+        <Link href={`movie-list/${id}`} className='p-4 my-2 flex flex-col items-center text-center justify-between'>
             <h1>{title}</h1>
             <h2>Release date: {release_date}</h2>
 
--- app/movie-list/[movie]/page.jsx
+++ app/movie-list/[movie]/page.jsx
@@ -6,14 +6,30 @@ export default async function MovieInfo({ params }) {
     const urlToFetch = `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
     const response = await fetch(urlToFetch);
     const data = await response.json();
-    
-    return(
+
+    return (
         <div className='m-4'>
             <h1 className="text-2xl">{data.title}</h1>
             <h2 className="text-lg">{data.release_date}</h2>
             <h2>Runtime: {data.runtime} minutes</h2>
             <h2 className="text-sm bg-green-600 inline-block my-2 p-2"> {data.status} </h2>
-            <Image alt={data.title} width={800} height={800} className= "my-12 w-full" src={img_prefix + data.backdrop_path} />
+            <Image alt={data.title} width={800} height={800} className="my-12 w-full" src={img_prefix + data.backdrop_path} />
         </div>
     )
+}
+
+
+// need to understand purpose for this function 👇
+export async function generateStaticParams() {
+    const urlToFetch = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
+    const response = await fetch(urlToFetch);
+    const data = await response.json();
+
+    return data.results.map((movie) => {
+        return {
+            params: {
+                movie: movie.id.toString()
+            }
+        }
+    });
 }
