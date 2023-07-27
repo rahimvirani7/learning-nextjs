a551f6255b7536b423c57a9b7d385d40aa0c0ed2
Rahim Virani <rahz616@gmail.com>
2023-07-27 13:10:36 -0400

    Running eslint format on files

--- app/movie-list/Movie.jsx
+++ app/movie-list/Movie.jsx
@@ -1,17 +1,25 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Movie({ title, id, poster_path, release_date }) {
-    const img_prefix = "https://image.tmdb.org/t/p/original";
-    // console.log(poster_path);
+  const img_prefix = "https://image.tmdb.org/t/p/original";
+  // console.log(poster_path);
 
-    return (
-        <Link href={`movie-list/${id}`} className='p-4 my-2 flex flex-col items-center text-center justify-between'>
-            <h1>{title}</h1>
-            <h2>Release date: {release_date}</h2>
+  return (
+    <Link
+      href={`movie-list/${id}`}
+      className="p-4 my-2 flex flex-col items-center text-center justify-between"
+    >
+      <h1>{title}</h1>
+      <h2>Release date: {release_date}</h2>
 
-            <Image src={img_prefix + poster_path}
-                width={300} height={300} alt={title} loading="lazy"></Image>
-        </Link>
-    )
+      <Image
+        src={img_prefix + poster_path}
+        width={300}
+        height={300}
+        alt={title}
+        loading="lazy"
+      ></Image>
+    </Link>
+  );
 }
--- app/movie-list/[movie]/page.jsx
+++ app/movie-list/[movie]/page.jsx
@@ -1,37 +1,45 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default async function MovieInfo({ params }) {
-    const { movie } = params;
-    const img_prefix = "https://image.tmdb.org/t/p/original";
-    const urlToFetch = `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
-    const response = await fetch(urlToFetch);
-    const data = await response.json();
+  const { movie } = params;
+  const img_prefix = "https://image.tmdb.org/t/p/original";
+  const urlToFetch = `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
+  const response = await fetch(urlToFetch);
+  const data = await response.json();
 
-    return (
-        <div className='m-4'>
-            <h1 className="text-2xl">{data.title}</h1>
-            <h2 className="text-lg">{data.release_date}</h2>
-            <h2>Runtime: {data.runtime} minutes</h2>
-            <h2 className="text-sm bg-green-600 inline-block my-2 p-2"> {data.status} </h2>
-            <Image alt={data.title} width={800} height={800} className="my-12 w-full" src={img_prefix + data.backdrop_path} />
-        </div>
-    )
+  return (
+    <div className="m-4">
+      <h1 className="text-2xl">{data.title}</h1>
+      <h2 className="text-lg">{data.release_date}</h2>
+      <h2>Runtime: {data.runtime} minutes</h2>
+      <h2 className="text-sm bg-green-600 inline-block my-2 p-2">
+        {" "}
+        {data.status}{" "}
+      </h2>
+      <Image
+        alt={data.title}
+        width={800}
+        height={800}
+        className="my-12 w-full"
+        src={img_prefix + data.backdrop_path}
+      />
+    </div>
+  );
 }
-
 
 // below fn serves as an implementation for getStaticPaths. It is responsible for generating the dynamic paths that will be pre-rendered at build time.
 export async function generateStaticParams() {
-    const urlToFetch = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
-    const response = await fetch(urlToFetch);
-    const data = await response.json();
+  const urlToFetch = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
+  const response = await fetch(urlToFetch);
+  const data = await response.json();
 
-    return data.results.map((movie) => {
-        return {
-            params: {
-                movie: movie.id.toString()
-            }
-        }
-    });
+  return data.results.map((movie) => {
+    return {
+      params: {
+        movie: movie.id.toString(),
+      },
+    };
+  });
 }
 
 /*
